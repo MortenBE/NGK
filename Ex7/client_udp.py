00bc15ea7b8784da1bdfa2d808fc35bb929719e3
Morten <ehlersmorten@gmail.com>
2020-03-12 16:22:31 +0100

    mindre ændringer på udp_client

--- Ex7/client_udp.py
+++ Ex7/client_udp.py
@@ -3,7 +3,7 @@ serverName = '10.0.0.2'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 
-message = input("Input lowercase sentence:")
+message = input("Input:")
 clientSocket.sendto(message.encode(),(serverName, serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 print(modifiedMessage.decode())
