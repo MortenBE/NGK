48c758014017d93bb2da402165cd618828ee6d48
Morten <Ehlersmorten@gmail.com>
2020-03-12 16:20:44 +0100

    Mindre ændringer på server_udp

--- Ex7/server_udp.py
+++ Ex7/server_udp.py
@@ -8,16 +8,18 @@ while True:
     message, clientAddress = serverSocket.recvfrom(2048)
 
     if message == b'u' or message == b'U':
-        print("Recieved U")
+        print("Recieved ", message)
         f = open('/proc/uptime','rb')
         l = f.read(1000)
         serverSocket.sendto(l, clientAddress)
 
     elif message == b'l' or message == b'L':
-        print("Recieved L")
+        print("Recieved ", message)
         f = open('/proc/loadavg','rb')
         l = f.read(1000)
         serverSocket.sendto(l, clientAddress)
 
     else:
+        print("Recieved ", message)
         print("No valid message recieved")
+        serverSocket.sendto("Error".encode(), clientAddress)
